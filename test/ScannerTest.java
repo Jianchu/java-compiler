608fda2a9dc2bedb864c8aed1dc7fdc54f047e3a
Jason <jianchuli0624@gmail.com>
2016-01-13 14:25:39 -0500

    test ignore

---
+++ test/ScannerTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ScannerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
