516fee934ff8ce0546e232cc1e01e88aee20cb20
Jianchu Li <j538li@uwaterloo.ca>
2016-01-14 09:47:06 -0500

    add Token class

---
+++ src/Token.java
@@ -0,0 +1,41 @@
+
+public class Token {
+
+    private final String lexeme;
+    private TokenType tokenType;
+
+    public Token(String lexeme, TokenType tokenType) {
+        this.lexeme = lexeme;
+        this.tokenType = tokenType;
+    }
+
+    // May be used for replacing id with keyword
+    public void setTokenType(TokenType tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public String getLexeme() {
+        return this.lexeme;
+    }
+
+    public TokenType getTokenType() {
+        return this.tokenType;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("<");
+        sb.append(this.lexeme);
+        sb.append(", ");
+        sb.append(tokenType.toString());
+        sb.append(">");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Token token = (Token) obj;
+        return this.lexeme.equals(token.lexeme)
+                && this.tokenType.equals(token.tokenType);
+    }
+}
