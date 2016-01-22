92e015997cbf9acf81f58fd948201dfdd6f2a5c2
Jianchu Li <j538li@uwaterloo.ca>
2016-01-21 19:42:12 -0500

    add test case

--- test/scannertest/TestCombo.java
+++ test/scannertest/TestCombo.java
@@ -21,7 +21,6 @@ public class TestCombo {
     public void testLangle() throws Exception {
         String in = "<<<<=<<=<=<<<";
         List<Token> tokens = scannerTest.inputSetUp(in);
-        scannerTest.printTokens(tokens);
         assertEquals(6, tokens.size());
         assertEquals("<<<, DBLANGLE>", tokens.get(0).toString());
         assertEquals("<<<=, LSHIFT_EQ>", tokens.get(1).toString());
@@ -51,7 +50,7 @@ public class TestCombo {
     */
 
     @Test
-    public void testIdplusId() throws Exception {
+    public void testNumplusNum() throws Exception {
         String in = "3 + 3";
         List<Token> tokens = scannerTest.inputSetUp(in);
         // scannerTest.printTokens(tokens);
@@ -59,6 +58,22 @@ public class TestCombo {
         assertEquals("<3, DECIMAL>", tokens.get(0).toString());
         assertEquals("<+, PLUS>", tokens.get(1).toString());
         assertEquals("<3, DECIMAL>", tokens.get(2).toString());
+    }
+
+    @Test
+    public void testVaildProgram() throws Exception {
+        String in = "public class A {\n public A() {}\n public int m1() {\n return 42;\n} public int m2() { \n return m1(); \n }\n}";
+        String in1 = "public class A { \npublic A() {}\npublic int m() {\nint[] x = new int[42];\nreturn x.length;\n}\n}";
+        String in2 = "public class A {public A() {}public boolean m(boolean x) {return (x & true) | !x;}}";
+        List<Token> tokens = scannerTest.inputSetUp(in1);
+        scannerTest.printTokens(tokens);
+    }
+    
+    @Test
+    public void t() throws Exception {
+        String in = ";";
+        List<Token> tokens = scannerTest.inputSetUp(in);
+        scannerTest.printTokens(tokens);
     }
 
 }
---
+++ test/testprogram/TestAll.java
@@ -0,0 +1,36 @@
+package testprogram;
+import java.util.Vector;
+import java.util.*;
+
+interface A {
+    
+}
+
+public class TestAll extends java.lang.Thread implements java.io.Serializable {
+    protected static int x_12;
+    protected int y12_;
+    
+    public TestAll() {
+
+    }
+
+    public static int m(java.util.Vector x) {
+        java.lang.System.out.println(x.size());
+        new TestAll().x_12 = 42;
+        int[] x123 = new int[42];
+        int LENGTG_length__123 =  x123.length;
+        return 42;
+    }
+    
+    public int m123() {
+        return x_12;
+    }
+    
+    public int m2() {
+        return m123();
+    }
+    
+    public int m() {
+        return java.util.Calendar.MONDAY;
+    }
+}
