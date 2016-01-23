1fd42838dc1a29b771154759925b35bdf2e3e6f9
Jianchu Li <j538li@uwaterloo.ca>
2016-01-23 17:08:32 -0500

    add all situation

--- test/testprogram/TestAll.java
+++ test/testprogram/TestAll.java
@@ -1,36 +1,80 @@
 package testprogram;
-import java.util.Vector;
-import java.util.*;
 
 interface A {
     
 }
 
-public class TestAll extends java.lang.Thread implements java.io.Serializable {
+public final class TestAll extends java.lang.Thread implements
+        java.io.Serializable {
     protected static int x_12;
-    protected int y12_;
-    
+    protected static int y12_;
+    public int a = 1;
+    Object A;
+    protected boolean x1 = (A) A instanceof A;
+    protected char x2 = 'c';
+    protected byte x3;
+    protected short x4;
+    protected int[] x5;
+    protected A x6 = null;
+    protected String x = "\b\t\n\f\r\"\'\\\064";
     public TestAll() {
+        this.x2 = 'e';
+    }
 
+    protected TestAll(int a) {
+        int a1 = (int) x3;
     }
 
     public static int m(java.util.Vector x) {
         java.lang.System.out.println(x.size());
         new TestAll().x_12 = 42;
         int[] x123 = new int[42];
+        x123[0] = -2 * y12_ + 87 % y12_ - (y12_ / 7);
         int LENGTG_length__123 =  x123.length;
         return 42;
     }
-    
-    public int m123() {
-        return x_12;
-    }
-    
+
     public int m2() {
-        return m123();
+
+        return m3();
+    }
+
+    public int[] foo() {
+        return null;
     }
     
-    public int m() {
-        return java.util.Calendar.MONDAY;
+    public final int m3() {
+        java.util.Vector x1 = null;
+        boolean x4 = true;
+        int x = 42;
+        while (x > 0)
+            x = x - 1;
+        int y = 87;
+        for (int i = x; i > 0; i = i - 1)
+            y = y + 1;
+
+        int a = x + 87;
+        if (x == 42) {
+            return 87;
+        } else if (x > 12) {
+            return x_12;
+        } else if ((x < 87) && (x > 42) && (x <= 86) && (x >= 43) && (x == 51)
+                && (x != 52) && !((x4 & true) | x4) && (x4 && true) || x4) {
+
+        } else {
+
+        }
+        {
+            int z = x + y;
+            return z;
+        }
+        
     }
+
+}
+
+abstract class abs {
+    public abstract int m();
+
+    public static native int m(int i);
 }
