29cf9d3741ec9ea17a5506ac4ccbbac7fcd6bbfa
Jianchu Li <j538li@uwaterloo.ca>
2016-01-27 21:10:20 -0500

    add Weeder class

---
+++ src/weeder/Weeder.java
@@ -0,0 +1,12 @@
+package weeder;
+
+import parser.ParseTree;
+
+public class Weeder {
+    final private ParseTree parseTree;
+
+    public Weeder(ParseTree parseTree) {
+        this.parseTree = parseTree;
+    }
+
+}
