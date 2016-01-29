2f4884749d260559dde42a6a1c059762890d6f52
Jianchu Li <j538li@uwaterloo.ca>
2016-01-28 21:42:32 -0500

    weed all restrictions

--- src/weeder/Weeder.java
+++ src/weeder/Weeder.java
@@ -25,9 +25,11 @@ public class Weeder {
         if (parseTree.getTokenType().equals(Symbol.ClassDeclaration)) {
             weed(parseTree, Symbol.ClassDeclaration);
         } else if (parseTree.getTokenType().equals(Symbol.MethodDeclaration)) {
-
+            weed(parseTree, Symbol.MethodDeclaration);
         } else if (parseTree.getTokenType().equals(Symbol.FieldDeclaration)) {
             weed(parseTree, Symbol.FieldDeclaration);
+        } else if (parseTree.getTokenType().equals(Symbol.InterfaceDeclaration)) {
+            weed(parseTree, Symbol.InterfaceDeclaration);
         }
         for (ParseTree child : parseTree.getChildren()) {
             visitParseTree(child);
@@ -39,6 +41,7 @@ public class Weeder {
         case ClassDeclaration:
             // System.out.println(node.getTokenType());
             visitClass(node);
+
             for (ParseTree child : node.getChildren()) {
                 if (child.getTokenType().equals(Symbol.Modifiers)) {
                     visitModifier(child, Symbol.ClassDeclaration);
@@ -46,9 +49,22 @@ public class Weeder {
             }
             break;
         case MethodDeclaration:
-
+            ParseTree methodHeader = null;
+            for (ParseTree child : node.getChildren()) {
+                // System.out.println(child.getTokenType());
+                if (child.getTokenType().equals(Symbol.MethodHeader)) {
+                    methodHeader = child;
+                    visitModifier(child, Symbol.MethodHeader);
+                } else if (child.getTokenType().equals(Symbol.MethodBody)) {
+                    visitModifier(methodHeader, Symbol.MethodDeclaration);
+                }
+            }
             break;
         case InterfaceDeclaration:
+            ParseTree methodHeaderNode = findNode(node, Symbol.MethodHeader);
+            if (methodHeaderNode.getTokenType().equals(Symbol.MethodHeader)) {
+                visitModifier(methodHeaderNode, Symbol.InterfaceDeclaration);
+            }
 
             break;
         case FieldDeclaration:
@@ -80,15 +96,33 @@ public class Weeder {
                     "Every class must contain at least one explicit constructor.");
         }
     }
+    
+    private ParseTree findNode(ParseTree node, Symbol goal) {
+        Stack st = new Stack();
+        st.push(node);
+        while (!st.isEmpty()) {
+            ParseTree currentNode = (ParseTree) st.pop();
+            for (ParseTree child : currentNode.getChildren()) {
+                if (child.getTokenType().equals(goal)) {
+                    return child;
+                }
+                st.push(child);
+            }
+        }
+        return node;
+    }
 
     private void visitModifier(ParseTree modifierNode, Symbol parent)
             throws WeedException {
+        System.out.println(modifierNode.getChildren());
+
         Stack st = new Stack();
         Set<Symbol> modifiersSet = new HashSet<Symbol>();
         st.push(modifierNode);
         while (!st.isEmpty()) {
             ParseTree currentNode = (ParseTree) st.pop();
             for (ParseTree child : currentNode.getChildren()) {
+                // System.out.println(child.getTokenType());
                 Symbol symbol = child.getTokenType();
                 if (modifiersSet.contains(symbol)) {
                     // Check: Duplicated modifer.
@@ -103,12 +137,42 @@ public class Weeder {
                 } else if (parent.equals(Symbol.FieldDeclaration) && symbol.equals(Symbol.FINAL)) {
                     // Check: No field can be final.
                     throw new WeedException("No field can be final.");
+                } else if (parent.equals(Symbol.MethodHeader)) {
+                    // Check: A static method cannot be final.
+                    if (symbol.equals(Symbol.STATIC) && modifiersSet.contains(Symbol.FINAL)) {
+                        throw new WeedException("A static method cannot be final.");
+                    } else if (symbol.equals(Symbol.FINAL) && modifiersSet.contains(Symbol.STATIC)) {
+                        throw new WeedException("A static method cannot be final.");
+                    }
+                } else if (parent.equals(Symbol.InterfaceDeclaration)) {
+                    if (symbol.equals(Symbol.STATIC)
+                            || symbol.equals(Symbol.FINAL)
+                            || symbol.equals(Symbol.NATIVE)) {
+                        throw new WeedException("An interface method cannot be static, final, or native.");
+                    }
                 }
                 if (!symbol.equals(Symbol.Modifiers) && !symbol.equals(Symbol.Modifier)) {
                     modifiersSet.add(symbol);
-                    System.out.println(child.getTokenType());
+                    // System.out.println(symbol);
                 }
                 st.push(child);
+            }
+        }
+        // Check: An abstract method cannot be static or final.
+        if (parent.equals(Symbol.MethodHeader)) {
+            if (modifiersSet.contains(Symbol.ABSTRACT)
+                    && (modifiersSet.contains(Symbol.STATIC))
+                    || modifiersSet.contains(Symbol.FINAL)) {
+                throw new WeedException("An abstract method cannot be static or final.");
+                // Check: A native method must be static.
+            } else if (modifiersSet.contains(Symbol.NATIVE)
+                    && !modifiersSet.contains(Symbol.STATIC)) {
+                throw new WeedException("A native method must be static.");
+            }
+        } else if (parent.equals(Symbol.MethodDeclaration)) {
+            // System.out.println(modifiersSet);
+            if (modifiersSet.contains(Symbol.ABSTRACT) || modifiersSet.contains(Symbol.NATIVE)) {
+                throw new WeedException("A method has a body if and only if it is neither abstract nor native.");
             }
         }
     }
