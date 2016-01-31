d949a3056bfc5864e841eca2d5bea144d813f216
Zhiyuan Lin <z72lin@uwaterloo.ca>
2016-01-31 16:30:02 -0500

    Block and variable delcaration

--- src/ast/MethodDeclaration.java
+++ src/ast/MethodDeclaration.java
@@ -1,5 +1,11 @@
 package ast;
 
-public class MethodDeclaration {
+import java.util.List;
+
+public class MethodDeclaration extends BodyDeclaration{
+	List<Modifier> modifiers;
+	Type returnType;	//what about void
 	
+	List<SingleVariableDeclaration> parameters;
+	Block body;
 }
