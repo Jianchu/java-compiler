cd881c164b5b5c760a252a8732238e76824d1d6b
Zhiyuan Lin <z72lin@uwaterloo.ca>
2016-01-31 15:22:49 -0500

    restructured files in parser

---
+++ src/parser/Action.java
@@ -0,0 +1,31 @@
+package parser;
+
+public class Action {
+	ShiftReduce act;
+	int actNum;
+	
+	public Action(String action, String num) throws Exception {
+		if (action.equals("shift")) {
+			act = ShiftReduce.SHIFT;
+		} else if (action.equals("reduce")) {
+			act = ShiftReduce.REDUCE;
+		} else {
+			throw new Exception("unexpected input.");
+		}
+		
+		actNum = Integer.parseInt(num);
+	}
+	
+	public ShiftReduce getShiftReduce() {
+		return act;
+	}
+	
+	public int getNum() {
+		return actNum;
+	}
+	
+	@Override
+	public String toString() {
+		return act.toString().toLowerCase() + " " + actNum;
+	}
+}
