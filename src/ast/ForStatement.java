f804f6f28ec17d85fcbe2b7a100de98889e77b4c
Jianchu Li <j538li@uwaterloo.ca>
2016-02-02 12:14:10 -0500

    for statement

--- src/ast/ForStatement.java
+++ src/ast/ForStatement.java
@@ -1,5 +1,46 @@
 package ast;
 
+import parser.ParseTree;
+import exceptions.ASTException;
+
 public class ForStatement extends Statement{
+
+    public Expression forInit;
+    public Expression forCondition;
+    public Expression forUpdate;
+    public Statement forBody;
+
+    public ForStatement(ParseTree forNode) throws ASTException {
+        for (ParseTree child : forNode.getChildren()) {
+            switch (child.getTokenType()) {
+            case ForInit:
+                break;
+            case Expression:
+                // Expression has not been implemented
+                // this.forCondition = Expression.(child);
+                break;
+            case ForUpdate:
+                break;
+            case Statement:
+            case StatementNoShortIf:
+                forBody = ASTBuilder.parseStatement(child);
+                break;
+            default:
+                throw new ASTException("Unexpected symbol");
+            }
+        }
+    }
+
+    public Expression getForCondition() {
+        return this.forCondition;
+    }
+
+    public Expression getForUpdate() {
+        return this.forUpdate;
+    }
+
+    public Statement getForBody() {
+        return this.forBody;
+    }
 
 }
