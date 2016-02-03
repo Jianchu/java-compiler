ddf3c8a6cb2ee3396a33f4b70c8358c432c9b828
Jianchu Li <j538li@uwaterloo.ca>
2016-02-02 21:27:02 -0500

    make some change in Joosc

--- src/Joosc.java
+++ src/Joosc.java
@@ -1,29 +1,41 @@
+import java.io.File;
 import java.io.FileReader;
 import java.util.List;
 
+import parser.ParseTree;
+import parser.Parser;
 import scanner.Scanner;
 import scanner.Token;
+import weeder.Weeder;
 
 public class Joosc {
     public static void main(String[] args) {
         if (args.length < 1) {
             System.err.println("Usage: java Joosc <filename>");
-            System.exit(-1);
+            System.exit(42);
         }
 
         Scanner scanner = null;
         List<Token> tokens = null;
+        File grammar;
         try {
-            scanner = new Scanner(new FileReader(args[0]));
+            grammar = new File(System.getProperty("user.dir") + "/data/grammar.lr1");
+            File input = new File(args[0]);
+            scanner = new Scanner(new FileReader(input));
             tokens = scanner.scan();
+            Parser parser = new Parser(tokens, grammar);
+            ParseTree parseTree = parser.parse();
+            Weeder weeder = new Weeder(parseTree, input.getName().substring(0, input.getName().lastIndexOf('.')));
+            weeder.weed();
         } catch (Exception e) {
             System.err.println(e);
-            System.exit(-1);
+            System.exit(42);
         }
+        System.exit(0);
 
         // for testing purposes
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        // for (Token token : tokens) {
+        // System.out.println(token);
+        // }
     }
 }
