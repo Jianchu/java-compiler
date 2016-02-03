07c44b3cbfa849347c29eebcb0205d50598bdb27
Jianchu Li <j538li@uwaterloo.ca>
2016-02-03 13:08:49 -0500

    expressionstatement

--- src/ast/ExpressionStatement.java
+++ src/ast/ExpressionStatement.java
@@ -1,5 +1,24 @@
 package ast;
 
+import parser.ParseTree;
+import scanner.Symbol;
+
 public class ExpressionStatement extends Statement{
+
+    private Expression statementExpression;
+
+    public ExpressionStatement(ParseTree expressionStatementNode) {
+        ParseTree StatementExpressionNode = ASTBuilder.findChild(
+                expressionStatementNode,
+                Symbol.StatementExpression);
+        if (StatementExpressionNode != null) {
+            statementExpression = ASTBuilder
+                    .parseExpression(StatementExpressionNode);
+        }
+    }
+
+    public Expression getStatementExpression() {
+        return this.statementExpression;
+    }
 
 }
