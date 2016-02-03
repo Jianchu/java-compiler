a7ce8895a40f5d54d6cd5865c5eb26d06ec4ee9d
Jianchu Li <j538li@uwaterloo.ca>
2016-02-03 13:01:42 -0500

    while statement

--- src/ast/WhileStatement.java
+++ src/ast/WhileStatement.java
@@ -1,5 +1,34 @@
 package ast;
 
+import parser.ParseTree;
+import exceptions.ASTException;
+
 public class WhileStatement extends Statement{
 
+    private Expression whileCondition;
+    private Statement whileStatement;
+
+    public WhileStatement(ParseTree whileNode) throws ASTException {
+        for (ParseTree child : whileNode.getChildren()) {
+            switch (child.getTokenType()) {
+            case Expression:
+                this.whileCondition = ASTBuilder.parseExpression(child);
+                break;
+            case Statement:
+            case StatementNoShortIf:
+                this.whileStatement = ASTBuilder.parseStatement(child);
+                break;
+            default:
+                throw new ASTException("Unexpected symbol");
+            }
+        }
+    }
+
+    public Expression getWhileCondition() {
+        return this.whileCondition;
+    }
+
+    public Statement getWhileStatement() {
+        return this.whileStatement;
+    }
 }
