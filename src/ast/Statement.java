bbb796e52457e9b1ba173eef88a0d743b707793e
Jianchu Li <j538li@uwaterloo.ca>
2016-02-03 20:16:34 -0500

    add statement not finish

--- src/ast/Statement.java
+++ src/ast/Statement.java
@@ -5,6 +5,7 @@ import scanner.Symbol;
 
 public abstract class Statement extends ASTNode {
 
+
     protected boolean checkNodeType(ParseTree node, Symbol symbol) {
         if (node.getTokenType().equals(symbol)) {
             return true;
@@ -12,4 +13,40 @@ public abstract class Statement extends ASTNode {
         return false;
     }
 
+    protected static Statement getStatement(ParseTree statementNode) {
+        Statement statement;
+        ParseTree realStatement = statementNode.getChildren().get(0);
+        switch (realStatement.getTokenType()) {
+        case IfThenStatement:
+        case IfThenElseStatement:
+        case IfThenElseStatementNoShortIf:
+            break;
+
+        case WhileStatement:
+        case WhileStatementNoShortIf:
+            break;
+        case ForStatementNoShortIf:
+        case ForStatement:
+            break;
+        case StatementWithoutTrailingSubstatement:
+
+        }
+        return null;
+    }
+
+    private static Statement getStatementNoTrailing(ParseTree statementNoTrailingNode) {
+        Statement statement;
+        ParseTree realStatement = statementNoTrailingNode.getChildren().get(0);
+        switch(realStatement.getTokenType()) {
+        case Block:
+            break;
+        case EmptyStatement:
+            break;
+        case ExpressionStatement:
+            break;
+        case ReturnStatement:
+            break;
+        }
+        return null;
+    }
 }
