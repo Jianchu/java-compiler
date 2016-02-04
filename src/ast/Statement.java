afa46c193c584e6a643b2ba110a301ed08e97843
Jianchu Li <j538li@uwaterloo.ca>
2016-02-03 22:25:31 -0500

    finish all parts of statements

--- src/ast/Statement.java
+++ src/ast/Statement.java
@@ -6,16 +6,11 @@ import exceptions.ASTException;
 
 public abstract class Statement extends ASTNode {
 
+    private static Statement statement = null;
+    private static Expression statementExpression = null;
 
-    protected boolean checkNodeType(ParseTree node, Symbol symbol) {
-        if (node.getTokenType().equals(symbol)) {
-            return true;
-        }
-        return false;
-    }
-
-    protected static Statement getStatement(ParseTree statementNode) throws ASTException {
-        Statement statement;
+    private static void visitStatement(ParseTree statementNode)
+            throws ASTException {
         ParseTree realStatement = statementNode.getChildren().get(0);
         switch (realStatement.getTokenType()) {
         case IfThenStatement:
@@ -34,12 +29,11 @@ public abstract class Statement extends ASTNode {
         case StatementWithoutTrailingSubstatement:
             statement = getStatementNoTrailing(realStatement);
         }
-        // should return null?
-        return null;
+        // return null for empty statement and "return;"
     }
 
     private static Statement getStatementNoTrailing(ParseTree statementNoTrailingNode) throws ASTException {
-        Statement statement;
+        Statement statement = null;
         ParseTree realStatement = statementNoTrailingNode.getChildren().get(0);
         switch(realStatement.getTokenType()) {
         case Block:
@@ -49,13 +43,34 @@ public abstract class Statement extends ASTNode {
             // return null?
             break;
         case ExpressionStatement:
+            ParseTree statementExpr = realStatement.findChild(Symbol.StatementExpression);
+            if (statementExpr != null) {
+                statementExpression = ASTBuilder.parseExpression(statementExpr);
+            }
             // call expression?
             break;
         case ReturnStatement:
             statement = new ReturnStatement(realStatement);
             break;
         }
-        // should return null?
-        return null;
+        // return null for empty statement and "return;"
+        return statement;
+    }
+
+    public static Statement getStatement(ParseTree statementNode) throws ASTException {
+        visitStatement(statementNode);
+        return statement;
+    }
+
+    public static Expression getStatementExpression(ParseTree statementNode) throws ASTException {
+        visitStatement(statementNode);
+        return statementExpression;
+    }
+
+    protected boolean checkNodeType(ParseTree node, Symbol symbol) {
+        if (node.getTokenType().equals(symbol)) {
+            return true;
+        }
+        return false;
     }
 }
