18ad42185c87b056b3ba68293bd4eeb70c73eb1b
Jianchu Li <j538li@uwaterloo.ca>
2016-02-18 00:05:36 -0500

    add some comment for visit QualifiedName

--- src/environment/TypeVisitor.java
+++ src/environment/TypeVisitor.java
@@ -1,5 +1,11 @@
 package environment;
 
+import ast.ArrayType;
+import ast.PrimitiveType;
+import ast.QualifiedName;
+import ast.SimpleName;
+import ast.SimpleType;
+
 /**
  * Responsible for type linking
  * @author zanel
@@ -7,4 +13,25 @@ package environment;
  */
 public class TypeVisitor extends SemanticsVisitor {
 
+    @Override
+    public void visit(SimpleName node) throws Exception {
+    }
+
+    // Fully qualified names are easy
+    @Override
+    public void visit(QualifiedName node) throws Exception {
+        // Search qualified name in global
+    }
+
+    @Override
+    public void visit(ArrayType node) throws Exception {
+    }
+
+    @Override
+    public void visit(PrimitiveType node) throws Exception {
+    }
+
+    @Override
+    public void visit(SimpleType node) throws Exception {
+    }
 }
