cd79a8ed61516d1bb8470ccaa91f02839a6dac37
Jianchu Li <j538li@uwaterloo.ca>
2016-02-18 23:08:01 -0500

    check unqualified names

--- src/environment/TypeVisitor.java
+++ src/environment/TypeVisitor.java
@@ -2,35 +2,63 @@ package environment;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import ast.ArrayType;
-import ast.PrimitiveType;
+import ast.ClassInstanceCreationExpression;
 import ast.QualifiedName;
 import ast.SimpleName;
 import ast.SimpleType;
 import ast.TypeDeclaration;
 import exceptions.TypeLinkException;
-
 /**
  * Responsible for type linking
  * @author zanel
  *
  */
-public class TypeVisitor extends SemanticsVisitor {
+public class TypeVisitor extends TopDeclVisitor {
     
     // globalPackages is the map between package name and the type names
-    // declared in that package
+    // declared in that package.
+    // May be useless here.
     private final Map<String, List<String>> globalPackages;
-    // global is the map between full name of a type and it's AST node
+    // global is the map between full name of a type and it's AST node.
     private final Map<String, TypeDeclaration> global;
     
-    public TypeVisitor() {
+    public TypeVisitor(SymbolTable syms) {
+        super(syms);
         this.globalPackages = SymbolTable.getAllPackages();
         this.global = SymbolTable.getGlobal();
     }
 
     @Override
     public void visit(SimpleName node) throws Exception {
+        // How can I know the type of current scope? Using null checking for
+        // now. See getCompUnitEnv.
+        Environment env = getCompUnitEnv(table.curr);
+        // Types are for enclosing class or interface? Key is full name?
+        if (!checkSimpleName(env.types.keySet(), node.toString())
+                && !checkSimpleName(env.singleImports.keySet(), node.toString())
+                && !checkSimpleName(env.samePackage.keySet(), node.toString())
+                && !checkSimpleName(env.importOnDemands.keySet(),node.toString())) {
+            throw new TypeLinkException("The type name is not found");
+        }
+    }
+
+    private boolean checkSimpleName(Set<String> fullNames, String simpleName) {
+        for (String fullName : fullNames) {
+            if (fullName.substring(fullName.lastIndexOf('.')).equals(simpleName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Environment getCompUnitEnv(Environment env) {
+        if (env.types == null) {
+            env = getCompUnitEnv(env.getEnclosing());
+        }
+        return env;
     }
 
     // Fully qualified names are easy
@@ -46,13 +74,22 @@ public class TypeVisitor extends SemanticsVisitor {
 
     @Override
     public void visit(ArrayType node) throws Exception {
-    }
-
-    @Override
-    public void visit(PrimitiveType node) throws Exception {
+        if (node.type != null) {
+            node.type.accept(this);
+        }
     }
 
     @Override
     public void visit(SimpleType node) throws Exception {
+        if (node.name != null) {
+            node.name.accept(this);
+        }
+    }
+    
+    @Override
+    public void visit(ClassInstanceCreationExpression node) throws Exception {
+        if (node.name != null) {
+            node.name.accept(this);
+        }
     }
 }
