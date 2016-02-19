1e2a13db7b10802252e69bb76e2cffb8ff8a46b3
Jianchu Li <j538li@uwaterloo.ca>
2016-02-18 21:15:28 -0500

    check qualified name

--- src/environment/TypeVisitor.java
+++ src/environment/TypeVisitor.java
@@ -1,10 +1,15 @@
 package environment;
 
+import java.util.List;
+import java.util.Map;
+
 import ast.ArrayType;
 import ast.PrimitiveType;
 import ast.QualifiedName;
 import ast.SimpleName;
 import ast.SimpleType;
+import ast.TypeDeclaration;
+import exceptions.TypeLinkException;
 
 /**
  * Responsible for type linking
@@ -12,6 +17,17 @@ import ast.SimpleType;
  *
  */
 public class TypeVisitor extends SemanticsVisitor {
+    
+    // globalPackages is the map between package name and the type names
+    // declared in that package
+    private final Map<String, List<String>> globalPackages;
+    // global is the map between full name of a type and it's AST node
+    private final Map<String, TypeDeclaration> global;
+    
+    public TypeVisitor() {
+        this.globalPackages = SymbolTable.getAllPackages();
+        this.global = SymbolTable.getGlobal();
+    }
 
     @Override
     public void visit(SimpleName node) throws Exception {
@@ -20,7 +36,12 @@ public class TypeVisitor extends SemanticsVisitor {
     // Fully qualified names are easy
     @Override
     public void visit(QualifiedName node) throws Exception {
+        String fullName = node.toString();
         // Search qualified name in global
+        if (!global.keySet().contains(fullName)) {
+            throw new TypeLinkException(
+                    "The full qualified type name is not found");
+        }
     }
 
     @Override
---
+++ src/exceptions/TypeLinkException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class TypeLinkException extends Exception {
+    public TypeLinkException(String errorMessage) {
+        super(errorMessage);
+    }
+}
