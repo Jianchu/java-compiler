618f4d29626ccc4151ea1ac259f28ea9ad9ca663
Zhiyuan Lin <z72lin@uwaterloo.ca>
2016-02-21 12:20:02 -0500

    method symbol

---
+++ src/environment/MethodSymbol.java
@@ -0,0 +1,14 @@
+package environment;
+
+import java.util.List;
+
+import ast.*;
+
+public class MethodSymbol {
+	private String id;
+	private List<TypeDeclaration> paramTypes;
+	
+	public MethodSymbol(MethodDeclaration md) {
+		
+	}
+}
