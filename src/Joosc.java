40100fa5a8a7f7c7ed9136fec124dfc4ff1cb168
Zhiyuan Lin <z72lin@uwaterloo.ca>
2016-02-26 21:59:52 -0500

    edits main method

--- src/Joosc.java
+++ src/Joosc.java
@@ -1,8 +1,10 @@
 import java.io.File;
 import java.io.FileReader;
+import java.util.LinkedList;
 import java.util.List;
 
 import ast.AST;
+import environment.SymbolTable;
 import parser.ParseTree;
 import parser.Parser;
 import scanner.Scanner;
@@ -15,29 +17,32 @@ public class Joosc {
             System.err.println("Usage: java Joosc <filename>");
             System.exit(42);
         }
-
+	
         Scanner scanner = null;
         List<Token> tokens = null;
         File grammar;
         try {
             grammar = new File(System.getProperty("user.dir") + "/data/grammar.lr1");
-            File input = new File(args[0]);
-            scanner = new Scanner(new FileReader(input));
-            tokens = scanner.scan();
-            Parser parser = new Parser(tokens, grammar);
-            ParseTree parseTree = parser.parse();
-            Weeder weeder = new Weeder(parseTree, input.getName().substring(0, input.getName().lastIndexOf('.')));
-            weeder.weed();
-			AST ast = new AST(parseTree);
+		    List<AST> trees = new LinkedList<AST>();
+		    for (String arg : args) {
+				File input = new File(arg);
+				scanner = new Scanner(new FileReader(input));
+				tokens = scanner.scan();
+				Parser parser = new Parser(tokens, grammar);
+				ParseTree parseTree = parser.parse();
+				Weeder weeder = new Weeder(parseTree, input.getName().substring(0, input.getName().lastIndexOf('.')));
+				weeder.weed();
+				AST ast = new AST(parseTree);
+				trees.add(ast);			
+		    }
+		    SymbolTable.buildEnvs(trees);
+		    
         } catch (Exception e) {
             System.err.println(e);
             System.exit(42);
         }
         System.exit(0);
 
-        // for testing purposes
-        // for (Token token : tokens) {
-        // System.out.println(token);
-        // }
     }
+    
 }
