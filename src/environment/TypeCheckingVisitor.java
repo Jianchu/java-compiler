8287b89dc7cb45dcfcb3a87643c483d817c7bf1f
Jianchu Li <j538li@uwaterloo.ca>
2016-03-02 15:47:22 -0500

    typecheckingvisitor

---
+++ src/environment/TypeCheckingVisitor.java
@@ -0,0 +1,92 @@
+package environment;
+
+import ast.ArrayAccess;
+import ast.ArrayCreationExpression;
+import ast.AssignmentExpression;
+import ast.BooleanLiteral;
+import ast.CastExpression;
+import ast.CharacterLiteral;
+import ast.ClassInstanceCreationExpression;
+import ast.FieldAccess;
+import ast.InfixExpression;
+import ast.InstanceofExpression;
+import ast.IntegerLiteral;
+import ast.MethodInvocation;
+import ast.NullLiteral;
+import ast.PrefixExpression;
+import ast.StringLiteral;
+import ast.ThisExpression;
+import ast.VariableDeclarationExpression;
+
+public class TypeCheckingVisitor extends TraversalVisitor {
+
+    // maybe need to add or delete some methods...
+
+    @Override
+    public void visit(ArrayAccess node) throws Exception {
+    }
+
+    @Override
+    public void visit(ArrayCreationExpression node) throws Exception {
+    }
+
+    @Override
+    public void visit(AssignmentExpression node) throws Exception {
+    }
+
+    @Override
+    public void visit(BooleanLiteral node) throws Exception {
+    }
+
+    @Override
+    public void visit(CastExpression node) throws Exception {
+    }
+
+    @Override
+    public void visit(CharacterLiteral node) throws Exception {
+    }
+
+    @Override
+    public void visit(ClassInstanceCreationExpression node) throws Exception {
+    }
+
+    @Override
+    public void visit(FieldAccess node) throws Exception {
+    }
+
+    @Override
+    public void visit(InfixExpression node) throws Exception {
+    }
+
+    @Override
+    public void visit(InstanceofExpression node) throws Exception {
+    }
+
+    @Override
+    public void visit(IntegerLiteral node) throws Exception {
+    }
+
+    @Override
+    public void visit(MethodInvocation node) throws Exception {
+    }
+
+    @Override
+    public void visit(NullLiteral node) throws Exception {
+    }
+
+    @Override
+    public void visit(PrefixExpression node) throws Exception {
+    }
+
+    @Override
+    public void visit(StringLiteral node) throws Exception {
+    }
+
+    @Override
+    public void visit(ThisExpression node) throws Exception {
+    }
+
+    @Override
+    public void visit(VariableDeclarationExpression node) throws Exception {
+    }
+}
