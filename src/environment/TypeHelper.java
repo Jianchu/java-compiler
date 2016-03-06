115efa22124524da9797b73e0f8fc339ba5ebc3b
h6fung <h6fung@student.cs.uwaterloo.ca>
2016-03-06 03:11:17 -0500

    TypeHelper

--- src/environment/TypeHelper.java
+++ src/environment/TypeHelper.java
@@ -1,39 +1,116 @@
 package environment;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 import ast.*;
 
 public class TypeHelper {
     // t1 := t2
     public static boolean assignable(Type t1, Type t2) {
+        if (t1 == null) {
+            return false;
+        }
+        if (t2 == null) {
+            return (t1 instanceof SimpleType);
+        }
+
         if (t1 instanceof PrimitiveType) {
-            if (t2 instanceof PrimitiveType) {
-            } else if (t2 instanceof SimpleType) {
-            } else if (t2 instanceof ArrayType) {
+            if (!(t2 instanceof PrimitiveType)) {
+                return false;
             }
+            PrimitiveType tau1 = (PrimitiveType) t1;
+            PrimitiveType tau2 = (PrimitiveType) t2;
+            return tau1.value == tau2.value ||
+                     (tau1.value == PrimitiveType.Value.SHORT && tau2.value == PrimitiveType.Value.BYTE) ||
+                     (tau1.value == PrimitiveType.Value.INT && tau2.value == PrimitiveType.Value.CHAR);
         } else if (t1 instanceof SimpleType) {
             if (t2 instanceof PrimitiveType) {
-            } else if (t2 instanceof SimpleType) {
-            } else if (t2 instanceof ArrayType) {
+                return false;
+            }
+            if (t2 instanceof SimpleType) {
+                SimpleType tau1 = (SimpleType) t1;
+                SimpleType tau2 = (SimpleType) t2;
+                return inheritsFrom(tau1, tau2);
+            }
+            if (t2 instanceof ArrayType) {
+                SimpleType tau1 = (SimpleType) t1;
+                return tau1.toString().equals("java.lang.Object") ||
+                         tau1.toString().equals("java.lang.Clonable") ||
+                         tau1.toString().equals("java.io.Serializable");
             }
         } else if (t1 instanceof ArrayType) {
-            if (t2 instanceof PrimitiveType) {
-            } else if (t2 instanceof SimpleType) {
-            } else if (t2 instanceof ArrayType) {
+            if (!(t2 instanceof ArrayType)) {
+                return false;
+            }
+            ArrayType tau1 = (ArrayType) t1;
+            ArrayType tau2 = (ArrayType) t2;
+            if (tau1.type instanceof PrimitiveType && tau2.type instanceof PrimitiveType) {
+                PrimitiveType tau1t = (PrimitiveType) tau1.type;
+                PrimitiveType tau2t = (PrimitiveType) tau2.type;
+                return tau1t.value == tau2t.value;
+            }
+            if (tau1.type instanceof SimpleType && tau2.type instanceof SimpleType) {
+                SimpleType tau1t = (SimpleType) tau1.type;
+                SimpleType tau2t = (SimpleType) tau2.type;
+                return inheritsFrom(tau1t, tau2t);
+            }
+            return false;
+        }
+
+        //we should never get here
+        return false;
+    }
+
+    private static boolean inheritsFrom(Type tau1, Type tau2) {
+        TypeDeclaration tDecl1 = tau1.getDeclaration();
+
+        if (!tDecl1.isInterface) {
+            TypeDeclaration tDecl2 = tau2.getDeclaration();
+            if (tDecl2.isInterface) {
+                return false;
+            }
+
+            for ( ; ; ) {
+                if (tDecl2.equals(tDecl1)) {
+                    return true;
+                }
+                if (tDecl2.superClass == null) {
+                    return false;
+                }
+                tDecl2 = tDecl2.superClass.getDeclaration();
             }
         }
-        /* tau := tau
-         short := byte
-         int L= char
-         sigma := tau; tau := rho -> sigma := rho
-         D <= C -> C := D
-         C := null
-        */
-        /*
-        Object := sigma []
-        Clonable := sigma []
-        java.io.Serializable := [
-         D <= C -> C[] := D[]
-        */
+
+        Set<TypeDeclaration> checked = new HashSet<>();
+        List<Type> toCheck = new LinkedList<>();
+        toCheck.add(tau2);
+
+        while (!toCheck.isEmpty()) {
+            TypeDeclaration tDecl = toCheck.remove(0).getDeclaration();
+            if (!tDecl.isInterface) {
+                if (tDecl.interfaces != null) {
+                    toCheck.addAll(tDecl.interfaces);
+                }
+                if (tDecl.superClass != null) {
+                    toCheck.add(tDecl.superClass);
+                }
+            }
+
+            if (checked.contains(tDecl)) {
+                continue;
+            }
+            if (tDecl.equals(tDecl1)) {
+                return true;
+            }
+            if (tDecl.interfaces != null) {
+                toCheck.addAll(tDecl.interfaces);
+            }
+            checked.add(tDecl);
+        }
+
         return false;
     }
 }
