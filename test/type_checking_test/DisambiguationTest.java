45dded695af053fc95e18a2dc98360f8039f5778
Jianchu Li <j538li@uwaterloo.ca>
2016-03-09 00:03:58 -0500

    add tests

--- test/type_checking_test/DisambiguationTest.java
+++ test/type_checking_test/DisambiguationTest.java
@@ -161,53 +161,45 @@ public class DisambiguationTest {
         assertEquals(0, result);
         }   
         
+        @Test
+        public void test12() {
+        String[] paths = new String[0];
+        paths = FileUtility.getFileNames(System.getProperty("user.dir") + "/assignment_testcases/a3/Je_16_ClosestMatch_Array.java").toArray(paths);
+        int result = Joosc.compileSTL(paths);
+        assertEquals(42, result);
+        }   
     /**
-     * J1_6_ProtectedAccess_InstanceField_This
-     * J2_6_ProtectedAccess_StaticField_This
-     * J1_6_ProtectedAccess_InstanceField_SubVar
-     * J2_6_ProtectedAccess_StaticField_Sub
-     * J1_6_ProtectedAccess_InstanceField_ThisVar
+     * Je_6_ProtectedAccess_InstanceField_SuperVar
+     * Je_6_ProtectedAccess_Method_OutsidePackage_NotInSubclass
+     * Je_6_ProtectedAccess_InstanceField_NoRelation_Internal
+     * Je_6_ProtectedAccess_Method_OutsidePackage_NotBySubclass
      * Je_6_ProtectedAccess_InstanceField_SubDeclare_SubVar
      * Je_6_ProtectedAccess_StaticMethod_Sub_DeclaredInSub
      * Je_6_ProtectedAccess_InstanceMethod_SuperVar
+     * Je_6_ProtectedAccess_ClassCreation_Sub
+     * Je_16_ProtectedAccess_StaticField_Sub_DeclaredInSub
+     * Je_6_ProtectedAccess_WriteField_OutsidePackage_NotInSubclass
+     * Je_6_ProtectedAccess_ReadField_OutsidePackage_NotBySubclass
      * Je_6_ProtectedAccess_ClassCreation_Super
      * Je_6_ProtectedAccess_ReadField_OutsidePackage_NotInSubclass
      * Je_6_ProtectedAccess_InstanceMethod_SubDeclare_SubVar
      * Je_6_ProtectedAccess_TwoSubtypes
-     * Je_6_ProtectedAccess_InstanceField_NoRelation_Internal
-     * Je_6_ProtectedAccess_Method_OutsidePackage_NotBySubclass
-     * Je_6_ProtectedAccess_InstanceField_SuperVar
-     * Je_6_ProtectedAccess_Method_OutsidePackage_NotInSubclass
      * Je_6_ProtectedAccess_WriteField_OutsidePackage_NotBySubclass
      * Je_6_ProtectedAccess_SuperConstructor_NewExp
-     * Je_6_ProtectedAccess_External Je_6_ProtectedAccess_ClassCreation_Sub
-     * Je_16_ProtectedAccess_StaticField_Sub_DeclaredInSub
-     * Je_6_ProtectedAccess_WriteField_OutsidePackage_NotInSubclass
+     * Je_6_ProtectedAccess_External 
      * Je_6_ProtectedAccess_Constructor
-     * Je_6_ProtectedAccess_ReadField_OutsidePackage_NotBySubclass
      * Je_6_ProtectedAccess_InstanceField_NoRelation_External
      * 
+     * Je_16_ClosestMatch_Array.java 
+     * Je_6_StaticThis_NonstaticField.java
+     * Je_6_StaticThis_NonStaticField_ImplicitThis.java
      * Je_6_StaticThis_InvokeNonStatic.java
      * Je_6_StaticThis_InvokeNonstatic_Implicit.java
      * Je_16_StaticThis_StaticFieldInitializer.java
-     * Je_6_StaticThis_AfterStaticInvoke.java
-     * Je_6_StaticThis_NonstaticField.java
-     * Je_6_StaticThis_NonStaticField_ImplicitThis.java
+     * Je_6_StaticThis_AfterStaticInvoke.java 
      * Je_6_StaticThis_InvokeStatic.java
-     * 
-     * Je_5_ForwardReference_FieldInOwnInitializer_ComplexExpression.java
-     * Je_5_ForwardReference_FieldInOwnInitializer_Direct.java
-     * Je_5_ForwardReference_FieldDeclaredLater_ComplexExp.java
-     * Je_5_ForwardReference_FieldInOwnInitializer_RightSideOfAssignment.java
-     * Je_5_ForwardReference_FieldInOwnInitializer_ReadAfterAssignment.java
-     * Je_5_ForwardReference_MethodCall.java
-     * Je_5_ForwardReference_InAssignment.java
-     * Je_5_ForwardReference_ArrayLength.java
-     * Je_5_ForwardReference_FieldDeclaredLater.java
-     * 
-     * Je_6_ConstructorPresent_Super_NoDefault
      * Je_6_NonStaticAccessToStatic_Method.java
-     * Je_5_AmbiguousName_FieldVsType_Initializer.java
-     * Je_6_NonStaticAccessToStatic_Field.java
+     * Je_6_StaticAccessToNontatic_Field.java
+     * Je_6_ConstructorPresent_Super_NoDefault
      */
 }
