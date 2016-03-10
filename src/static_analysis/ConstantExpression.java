4c1b749adedf06228940161ee4d8c49957c48565
h6fung <h6fung@student.cs.uwaterloo.ca>
2016-03-10 16:36:29 -0500

    ConstantExpression stub

---
+++ src/static_analysis/ConstantExpression.java
@@ -0,0 +1,9 @@
+package static_analysis
+
+
+public class ConstantExpression {
+    //see section 15.28 of JLS
+    public static boolean isConstant(Expression expr) {
+        return false;
+    }
+}
