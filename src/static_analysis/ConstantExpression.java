fc40a8b9b1687d252ab677cdfca14f2dd4d0ad76
h6fung <h6fung@student.cs.uwaterloo.ca>
2016-03-10 16:53:40 -0500

    update ConstantExpression stub

--- src/static_analysis/ConstantExpression.java
+++ src/static_analysis/ConstantExpression.java
@@ -2,8 +2,12 @@ package static_analysis
 
 
 public class ConstantExpression {
+
+    static int NOT_CONSTANT = 0;
+    static int CONSTANT_TRUE = 1;
+    static int CONSTANT_FALSE = 2;
     //see section 15.28 of JLS
-    public static boolean isConstant(Expression expr) {
-        return false;
+    public static int isConstant(Expression expr) {
+        return NOT_CONSTANT;
     }
 }
