99fd783dc5c6d673a2e48a1b6c75e09019c1db78
Jianchu Li <j538li@uwaterloo.ca>
2016-03-10 19:54:48 -0500

    add all statements

--- src/static_analysis/ReachabilityVisitor.java
+++ src/static_analysis/ReachabilityVisitor.java
@@ -1,7 +1,48 @@
 package static_analysis;
 
+import ast.Block;
+import ast.ExpressionStatement;
+import ast.ForStatement;
+import ast.IfStatement;
+import ast.ReturnStatement;
+import ast.VariableDeclarationStatement;
+import ast.WhileStatement;
 import environment.TraversalVisitor;
 
 public class ReachabilityVisitor extends TraversalVisitor {
 
+    @Override
+    public void visit(Block node) throws Exception {
+        super.visit(node);
+    }
+
+    @Override
+    public void visit(ExpressionStatement node) throws Exception {
+        super.visit(node);
+    }
+
+    @Override
+    public void visit(ForStatement node) throws Exception {
+        super.visit(node);
+    }
+
+    @Override
+    public void visit(IfStatement node) throws Exception {
+        super.visit(node);
+    }
+
+    @Override
+    public void visit(ReturnStatement node) throws Exception {
+        super.visit(node);
+    }
+
+    @Override
+    public void visit(VariableDeclarationStatement node) throws Exception {
+        super.visit(node);
+    }
+
+    @Override
+    public void visit(WhileStatement node) throws Exception {
+        super.visit(node);
+    }
 }
