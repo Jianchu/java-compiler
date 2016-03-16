830ab990379fa32124c934055a8f8f0b2cee1d04
Jianchu Li <j538li@uwaterloo.ca>
2016-03-16 17:48:23 -0400

    add method sig

--- src/code_generation/SigHelper.java
+++ src/code_generation/SigHelper.java
@@ -1,14 +1,19 @@
 package code_generation;
 
+import ast.ASTNode;
 import ast.ArrayType;
+import ast.MethodDeclaration;
 import ast.PrimitiveType;
 import ast.PrimitiveType.Value;
+import ast.SimpleName;
 import ast.SimpleType;
 import ast.Type;
+import ast.TypeDeclaration;
+import ast.VariableDeclaration;
 
 public class SigHelper {
 
-    public static String GetTypeSig(Type type) {
+    public static String getTypeSig(Type type) {
         String sigName = null;
         if (type instanceof SimpleType) {
             SimpleType stype = (SimpleType)type;
@@ -22,7 +27,7 @@ public class SigHelper {
             }
         } else if (type instanceof ArrayType) {
             ArrayType atype = (ArrayType) type;
-            String arrayTypeName = GetTypeSig(atype.type);
+            String arrayTypeName = getTypeSig(atype.type);
             if (atype.type instanceof SimpleType) {
                 sigName = "[L" + arrayTypeName;
             } else {
@@ -30,5 +35,35 @@ public class SigHelper {
             }
         }
         return sigName;
+    }
+
+    public static String getMethodSig(MethodDeclaration md) {
+        String classSig = null;
+        StringBuilder methodSig = new StringBuilder();
+        ASTNode typeNode = md.getParent();
+        if (typeNode instanceof TypeDeclaration) {
+            TypeDeclaration typeDec = (TypeDeclaration) typeNode;
+            String name = typeDec.getFullName();
+            SimpleType simpleType = new SimpleType(new SimpleName(name));
+            classSig = getTypeSig(simpleType);
+        }
+        methodSig.append(classSig);
+        if (md.isConstructor) {
+            methodSig.append("/<init>(");
+        } else {
+            methodSig.append("/m(");
+        }
+        if (md.parameters != null) {
+            for (VariableDeclaration varDec : md.parameters) {
+                methodSig.append(getTypeSig(varDec.type));
+            }
+        }
+        methodSig.append(")");
+        if (md.isConstructor) {
+            methodSig.append("V");
+        } else {
+            methodSig.append(getTypeSig(md.returnType));
+        }
+        return methodSig.toString();
     }
 }
