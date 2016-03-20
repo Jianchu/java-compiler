f7e1cfe273513016a47aca374e42869dc68cd1a3
zane <zane.z.lin@gmail.com>
2016-03-20 17:32:29 -0400

    simple test

--- src/code_generation/CodeGenUtil.java
+++ src/code_generation/CodeGenUtil.java
@@ -1,5 +1,6 @@
 package code_generation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import utility.StringUtility;
@@ -17,7 +18,17 @@ public class CodeGenUtil {
 			StringUtility.appendLine(sb, "push " + p + "\t; pushing parameter " + i);
 		}
 		StringUtility.appendLine(sb, "call " + subLabel);
-		StringUtility.appendLine(sb, "add esp " + params.size() + "\t; caller cleanup");	
+		StringUtility.appendLine(sb, "add esp " + params.size() + "*4" + "\t; caller cleanup");	
 		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		String fn = "multiply";
+		List<String> params = new ArrayList<String>();
+		params.add("[a]");
+		params.add("[b]");
+		System.out.println(fnCall(fn, params));
+		
+		
 	}
 }
