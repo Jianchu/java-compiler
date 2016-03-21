bcb3a6521d2c1084e68b3f79905b7b01531e6601
Jianchu Li <j538li@uwaterloo.ca>
2016-03-21 19:53:00 -0400

    field in data

--- src/code_generation/CodeGenerator.java
+++ src/code_generation/CodeGenerator.java
@@ -4,6 +4,7 @@ package code_generation;
 import utility.StringUtility;
 import ast.FieldDeclaration;
 import ast.Modifier;
+import ast.TypeDeclaration;
 import ast.WhileStatement;
 import environment.TraversalVisitor;
 
@@ -16,6 +17,39 @@ public class CodeGenerator extends TraversalVisitor {
         expGen = new ExpressionCodeGenerator();
     }
 
+    public void visit(TypeDeclaration node) throws Exception {
+        StringBuilder dataSection = new StringBuilder();
+        // creating .data section
+        StringUtility.appendLine(dataSection, "section .data");
+        for (FieldDeclaration fDecl : node.getEnvironment().fields.values()) {
+            putFieldInData(dataSection, fDecl);
+        }
+        for (FieldDeclaration fDecl : node.getEnvironment().getEnclosing().fields.values()) {
+            putFieldInData(dataSection, fDecl);
+            // inherited fields
+        }
+
+        // creating .text section
+        for (FieldDeclaration fDecl : node.getEnvironment().fields.values()) {
+            fDecl.accept(this);
+
+        }
+        for (FieldDeclaration fDecl : node.getEnvironment().getEnclosing().fields.values()) {
+            fDecl.accept(this);
+
+            // inherited fields
+
+        }
+        // methods
+    }
+    
+    private void putFieldInData(StringBuilder sb, FieldDeclaration fDecl) {
+        String fieldSig = SigHelper.getFieldSig(fDecl);
+        StringUtility.appendLine(sb, "global " + fieldSig + "\t; define global label for field");
+        StringUtility.appendLine(sb, fieldSig + ":" + "\t; label start");
+        StringUtility.appendLine(sb, "\t" + "dw 0x0" + "\t; default value: 0 false null");
+    }
+
     public void visit(FieldDeclaration node) throws Exception {
         String fieldSig = SigHelper.getFieldSig(node);
         StringBuilder fieldAssemblyText = new StringBuilder();
@@ -24,15 +58,9 @@ public class CodeGenerator extends TraversalVisitor {
             im.accept(this);
         }
         
-        if (node.modifiers.contains(Modifier.STATIC)) {
-            StringUtility.appendLine(fieldAssemblyText, "global " + fieldSig + "\t; define global label for field");
-            StringUtility.appendLine(fieldAssemblyText, fieldSig + ":" + "\t; label start");
-            StringUtility.appendLine(fieldAssemblyText, "\t" + "dw 0x0" + "\t; default value: 0 false null");
-        }
-        
         node.type.accept(this);
         if (node.initializer != null) {
-            node.initializer.accept(this);
+            node.initializer.accept(expGen);
             String initCode = node.initializer.getCode();
             StringUtility.appendLine(fieldAssemblyText, initCode);
             StringUtility.appendLine(fieldAssemblyText, "mov dword [" + fieldSig + "], eax" + "/t; initiallize field");
