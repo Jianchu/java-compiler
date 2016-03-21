e506488066c5ce839c63cedd401e2936ae52ec7a
Zhiyuan Lin <z72lin@uwaterloo.ca>
2016-03-21 14:47:41 -0400

    calling convention

--- src/code_generation/CodeGenUtil.java
+++ src/code_generation/CodeGenUtil.java
@@ -8,22 +8,53 @@ import utility.StringUtility;
 public class CodeGenUtil {
 	
 	/**
-	 * follows cdecl call convention
+	 * follows thisdecl calling convention from c++, which is similar to cdecl except that at offset zero is the pointer to object
+	 * Caller cleans up arguments after the call
 	 * @return
 	 */
-	public static String fnCall(String subLabel, List<String> params) {
+	public static String fnCall(String subLabel, String obj, List<String> params) {
 		StringBuilder sb = new StringBuilder();
-		StringUtility.appendLine(sb, "push ebp");
-		StringUtility.appendLine(sb, "mov ebp esp");
 		
 		for (int i = params.size() -1; i >= 0; i--) {
 			String p = params.get(i);
-			StringUtility.appendLine(sb, "push " + p + "\t; pushing parameter " + i);
+			StringUtility.appendIndLn(sb, "push " + p + "\t; pushing parameter " + i);
 		}
-		StringUtility.appendLine(sb, "call " + subLabel);
-		//StringUtility.appendLine(sb, "add esp " + params.size() + "*4" + "\t; caller cleanup");	
-		StringUtility.appendLine(sb, "mov esp ebp \t; caller cleanup");
-		StringUtility.appendLine(sb, "pop ebp ; restore to previosu frame.");
+		
+		StringUtility.appendIndLn(sb, "push " + obj + "\t; push object to offset zero");
+		
+		StringUtility.appendIndLn(sb, "call " + subLabel);
+		StringUtility.appendIndLn(sb, "add esp " + (params.size()+1) + "*4" + "\t; caller cleanup arguments.");	
+		
+		return sb.toString();
+	}
+	
+	/**
+	 * Template for the body of a subroutine.
+	 * [ebp] is the old ebp address
+	 * [ebp + 4] is the address automatically pushed by the call instruction 
+	 * [ebp + 8] is the address for the object this method belongs to. similar to function(self) in python
+	 * [ebp + 3 * 4] is the first parameter, [ebp + 4 * 4s] the second
+	 * 
+	 * @param fnName
+	 * @param fnBody
+	 * @return
+	 */
+	public static String fnDecl(String fnName, String fnBody, int maxOffSet) {
+		StringBuilder sb = new StringBuilder();
+		
+		StringUtility.appendLine(sb, fnName + ": \t; label for subroutine");
+		
+		StringUtility.appendIndLn(sb, "push ebp \t; save old frame pointer");
+		StringUtility.appendIndLn(sb, "mov ebp esp \t; move ebp to top of stack");
+		
+		StringUtility.appendIndLn(sb, "sub esp " + maxOffSet + "\t; space for local variables");
+		
+		StringUtility.appendIndLn(sb, fnBody);
+		
+		StringUtility.appendIndLn(sb, "add esp " + maxOffSet + "\t; pop local variables");
+		StringUtility.appendIndLn(sb, "pop ebp \t; restore to previosu frame.");
+		
+		StringUtility.appendIndLn(sb, "ret \t; end of subroutine");
 		return sb.toString();
 	}
 	
@@ -32,6 +63,10 @@ public class CodeGenUtil {
 		List<String> params = new ArrayList<String>();
 		params.add("[a]");
 		params.add("[b]");
-		System.out.println(fnCall(fn, params));
+		System.out.println(fnCall(fn, "eax", params));
+		
+		System.out.println();
+		
+		System.out.println(fnDecl(fn, "...body...", 8));
 	}
 }
--- src/utility/StringUtility.java
+++ src/utility/StringUtility.java
@@ -6,4 +6,16 @@ public class StringUtility {
         sb.append(s);
         sb.append("\n");
     }
+    
+    public static void appendLine(StringBuilder sb, String s, int indent) {
+    	String ind = "";
+    	for (int i = 0; i < indent; i++) {
+    		ind += "\t";
+    	}
+    	appendLine(sb, ind + s);
+    }
+    
+    public static void appendIndLn(StringBuilder sb, String s) {
+    	appendLine(sb, s, 1);
+    }
 }
