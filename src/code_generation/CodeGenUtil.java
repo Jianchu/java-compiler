5f39e45883a9f2b729307661576e33438c5afe54
zane <zane.z.lin@gmail.com>
2016-03-20 20:51:57 -0400

    manage ebp esp

--- src/code_generation/CodeGenUtil.java
+++ src/code_generation/CodeGenUtil.java
@@ -13,12 +13,17 @@ public class CodeGenUtil {
 	 */
 	public static String fnCall(String subLabel, List<String> params) {
 		StringBuilder sb = new StringBuilder();
+		StringUtility.appendLine(sb, "push ebp");
+		StringUtility.appendLine(sb, "mov ebp esp");
+		
 		for (int i = params.size() -1; i >= 0; i--) {
 			String p = params.get(i);
 			StringUtility.appendLine(sb, "push " + p + "\t; pushing parameter " + i);
 		}
 		StringUtility.appendLine(sb, "call " + subLabel);
-		StringUtility.appendLine(sb, "add esp " + params.size() + "*4" + "\t; caller cleanup");	
+		//StringUtility.appendLine(sb, "add esp " + params.size() + "*4" + "\t; caller cleanup");	
+		StringUtility.appendLine(sb, "mov esp ebp \t; caller cleanup");
+		StringUtility.appendLine(sb, "pop ebp ; restore to previosu frame.");
 		return sb.toString();
 	}
 	
@@ -28,7 +33,5 @@ public class CodeGenUtil {
 		params.add("[a]");
 		params.add("[b]");
 		System.out.println(fnCall(fn, params));
-		
-		
 	}
 }
