7ef8e1240b52fcdb55db0489b47eef47b5a36dd2
Zhiyuan Lin <z72lin@uwaterloo.ca>
2016-03-25 15:46:57 -0400

    local var offset

--- src/code_generation/VariableOffSetVisitor.java
+++ src/code_generation/VariableOffSetVisitor.java
@@ -1,15 +1,21 @@
 package code_generation;
 
+import ast.Block;
 import ast.MethodDeclaration;
 import ast.Modifier;
+import ast.Statement;
 import ast.VariableDeclaration;
 import environment.TraversalVisitor;
 
 public class VariableOffSetVisitor extends TraversalVisitor{
-    
+    int offsetCounter = 0;	// local variable offset starts from 0 
+    MethodDeclaration currMethod;
+	
 	@Override
 	public void visit(MethodDeclaration node) throws Exception {
-        for (Modifier mo : node.modifiers) {
+		currMethod = node;
+		
+		for (Modifier mo : node.modifiers) {
             mo.accept(this);
         }
         if (!node.isConstructor) {
@@ -27,7 +33,33 @@ public class VariableOffSetVisitor extends TraversalVisitor{
         }
         
         if (node.body != null) {
+        	offsetCounter = 0;	// initialize
             node.body.accept(this);
+            offsetCounter = 0;	// reset
         }
+        
+        currMethod = null;
+    }
+	
+	@Override
+    public void visit(Block node) throws Exception {
+		int oldCounter = offsetCounter;	// store for later
+		if (node.statements.size() > 0) {
+			Statement first = node.statements.get(0);
+			first.accept(this);
+		}
+		offsetCounter = oldCounter;
+    }
+	
+	@Override
+    public void visit(VariableDeclaration node) throws Exception {
+        node.type.accept(this);
+        if (node.initializer != null) {
+            node.initializer.accept(this);
+        }
+        
+        // store offset
+        currMethod.addVarOffSet(node, offsetCounter);
+        offsetCounter ++;
     }
 }
