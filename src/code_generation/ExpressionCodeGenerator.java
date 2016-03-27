c187689579765030a31f4537420d937236f85fbf
zane <zane.z.lin@gmail.com>
2016-03-27 15:50:59 -0400

    Primary.ID()

--- src/code_generation/ExpressionCodeGenerator.java
+++ src/code_generation/ExpressionCodeGenerator.java
@@ -3,17 +3,27 @@ package code_generation;
 import utility.StringUtility;
 import ast.BooleanLiteral;
 import ast.CharacterLiteral;
+import ast.Expression;
 import ast.IntegerLiteral;
+import ast.MethodDeclaration;
+import ast.MethodInvocation;
 import ast.NullLiteral;
 import ast.StringLiteral;
+import ast.TypeDeclaration;
+import environment.NameHelper;
 import environment.TraversalVisitor;
+import exceptions.NameException;
 
 public class ExpressionCodeGenerator extends TraversalVisitor {
 
     private static final String FALSE = "0x0";
     private static final String TRUE = "0xffffffff";
     private int stringLitCounter = 0;
-
+    
+    /*
+     * (non-Javadoc)
+     * Literals
+     */
     // String is Object.
     public void visit(StringLiteral node) throws Exception {
         // can use counter because string literal is not global.
@@ -58,4 +68,60 @@ public class ExpressionCodeGenerator extends TraversalVisitor {
         intText = "mov eax, " + node.value;
         node.attachCode(intText);
     }
+    
+    /*
+     * 
+     */
+    @Override
+    public void visit(MethodInvocation node) throws Exception {
+    	StringBuilder sb = new StringBuilder();
+
+    	// generate code for arguments
+    	int numArgs;
+    	for (numArgs =0; numArgs < node.arglist.size() ; numArgs++) {
+    		Expression expr = node.arglist.get(numArgs);
+    		expr.accept(this);
+    		StringUtility.appendIndLn(sb, expr.getCode());
+    		StringUtility.appendIndLn(sb, "push eax \t; push argument " + numArgs);
+    	}
+    	
+    	if (node.id != null) {
+    		// Primary.ID(...)
+    		node.expr.accept(this);	// generate code for Primary expression
+    		StringUtility.appendIndLn(sb, node.getCode());	// by this point eax should contain address to object return by Primary expression
+    		StringUtility.appendIndLn(sb, "push eax \t; push object for method invocation");
+    		MethodDeclaration mDecl = (MethodDeclaration) node.id.getDeclaration();	// the actual method being called
+    		
+    		// call method
+    		StringUtility.appendIndLn(sb, generateMethodCall(mDecl));
+    		
+    		// clean up
+    		StringUtility.appendIndLn(sb, "add esp " + (numArgs+1) + "*4" + "\t; caller cleanup arguments.");	
+    	} else {
+    		// Name(...)
+    		
+    	}
+    }
+    
+    /**
+     * generating actual method call by offset, assumes that the object is in eax
+     * @param mDecl
+     * @return
+     * @throws NameException
+     * @throws Exception
+     */
+    private String generateMethodCall(MethodDeclaration mDecl) throws NameException, Exception {
+    	String call;
+    	TypeDeclaration tDecl = (TypeDeclaration) mDecl.getParent();
+		// generate method call
+		if (tDecl.isInterface) {	// interface method
+			int offset = OffSet.getInterfaceMethodOffset(NameHelper.mangle(mDecl));
+			call = "call [[eax] + " + offset + "*4] \t; call interface method.";		//TODO: check if the level of indirection is proper 
+		} else {
+			int offset = tDecl.getMethodOffSet(NameHelper.mangle(mDecl));
+			call = "call [[eax] + " + offset + "*4] \t; call class method.";
+		}
+		return call;
+    }
+    
 }
