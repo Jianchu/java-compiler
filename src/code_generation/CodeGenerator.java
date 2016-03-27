b7ee948b557bcad9cb7ccf1cc5408ec8989385fa
Jianchu Li <j538li@uwaterloo.ca>
2016-03-27 14:59:25 -0400

    static field

--- src/code_generation/CodeGenerator.java
+++ src/code_generation/CodeGenerator.java
@@ -19,7 +19,9 @@ public class CodeGenerator extends TraversalVisitor {
     StatementCodeGenerator stmtGen;
     ExpressionCodeGenerator expGen;
     Map<Integer, String> SigOffsets = new HashMap<Integer, String>();
+    private static StringBuilder[] staticFieldInit = {new StringBuilder(), new StringBuilder()};
     static boolean debug = true;
+
     public CodeGenerator() {
         stmtGen = new StatementCodeGenerator();
         expGen = new ExpressionCodeGenerator();
@@ -84,11 +86,6 @@ public class CodeGenerator extends TraversalVisitor {
         for (MethodDeclaration mDecl: node.getEnvironment().methods.values()) {
             mDecl.accept(this);
         }
-        
-        // for (MethodDeclaration mDecl:
-        // node.getEnvironment().getEnclosing().methods.values()) {
-        // mDecl.accept(this);
-        // }
     }
     
     private void staticMethodVTableHandler(Map.Entry<String, MethodDeclaration> entry, TypeDeclaration node, StringBuilder textSection) throws Exception {
@@ -108,6 +105,8 @@ public class CodeGenerator extends TraversalVisitor {
 
     private void putFieldInData(StringBuilder sb, FieldDeclaration fDecl, TypeDeclaration typeDec) {
         String fieldSig = SigHelper.getStaticFieldSig(typeDec, fDecl);
+        String fieldSigInDec = SigHelper.getFieldSig(fDecl);
+        StringUtility.appendLine(staticFieldInit[0], "call static_init_" + fieldSigInDec, 2);
         StringUtility.appendLine(sb, "global " + fieldSig + "\t; define global label for field");
         StringUtility.appendLine(sb, fieldSig + ":" + "\t; label start");
         StringUtility.appendLine(sb, "\t" + "dw 0x0" + "\t; default value: 0 false null");
@@ -116,17 +115,26 @@ public class CodeGenerator extends TraversalVisitor {
     public void visit(FieldDeclaration node) throws Exception {
         String fieldSig = SigHelper.getFieldSig(node);
         StringBuilder fieldAssemblyText = new StringBuilder();
-        
         for (Modifier im : node.modifiers) {
             im.accept(this);
         }
         
         node.type.accept(this);
+        
         if (node.initializer != null) {
             node.initializer.accept(expGen);
             String initCode = node.initializer.getCode();
-            StringUtility.appendLine(fieldAssemblyText, initCode);
-            StringUtility.appendLine(fieldAssemblyText, "mov dword [" + fieldSig + "], eax" + "/t; initiallize field");
+            if (initCode == null) {
+                initCode = "; no right hand side yet.";
+            }
+            if (node.modifiers.contains(Modifier.STATIC)) {
+                StringUtility.appendIndLn(staticFieldInit[1], "static_init_" + fieldSig + ":");
+                StringUtility.appendLine(staticFieldInit[1], initCode, 2);
+                StringUtility.appendLine(staticFieldInit[1], "mov " + fieldSig + " eax", 2);
+            } else {
+                StringUtility.appendLine(fieldAssemblyText, initCode);
+                StringUtility.appendLine(fieldAssemblyText, "mov dword [" + fieldSig + "], eax" + "/t; initiallize field");
+            }
         }
         node.attachCode(fieldAssemblyText.toString());
     }
@@ -135,16 +143,20 @@ public class CodeGenerator extends TraversalVisitor {
         node.accept(stmtGen);
     }
 
+    protected static String getStaticFieldInit() {
+        return staticFieldInit[0].toString() + staticFieldInit[1].toString();
+    }
+
     public static void generate(List<AST> trees) throws Exception {
         for (AST t : trees) {
             Visitor rv = new CodeGenerator();
             if (debug) {
-                if (t.root.types.get(0).getFullName().contains("String")) {
+                if (t.root.types.get(0).getFullName().contains("Byte")) {
                     System.out.println(t.root.types.get(0).getFullName().toString());
                     t.root.accept(rv);
                 }
             } else {
-                t.root.accept(rv);
+                // t.root.accept(rv);
             }
         }
     }
--- src/code_generation/CodePrinter.java
+++ src/code_generation/CodePrinter.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
+import utility.StringUtility;
+
 public class CodePrinter {
 
     private static final File output = new File(System.getProperty("user.dir") + "/output");
     private static final String uglyText = UglyTableBuilder.getUgly();
+    private static final String staticFieldInit = CodeGenerator.getStaticFieldInit();
 
     public static void printCode() throws FileNotFoundException {
         if (!output.exists()) {
@@ -18,6 +21,7 @@ public class CodePrinter {
             }
         }
         wirteUgly();
+        wirteStaticFieldInit();
     }
 
     private static void wirteUgly() throws FileNotFoundException {
@@ -27,4 +31,15 @@ public class CodePrinter {
         writer.close();
     }
 
+    private static void wirteStaticFieldInit() throws FileNotFoundException {
+        File staticFieldInitFile = new File(output.getAbsolutePath() + "/staticinit.s");
+        PrintWriter writer = new PrintWriter(staticFieldInitFile);
+        StringBuilder fileHead = new StringBuilder();
+        StringUtility.appendLine(fileHead, "section. text");
+        StringUtility.appendLine(fileHead, "gloabl static_init");
+        StringUtility.appendIndLn(fileHead, "static_init:");
+        writer.write(fileHead.toString());
+        writer.write(staticFieldInit);
+        writer.close();
+    }
 }
