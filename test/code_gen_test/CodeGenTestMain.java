37f94f5e414a1fba73bb89e93d5f8184c236173f
Jianchu Li <j538li@uwaterloo.ca>
2016-03-29 19:06:39 -0400

    call runnasm.sh

--- test/code_gen_test/CodeGenTestMain.java
+++ test/code_gen_test/CodeGenTestMain.java
@@ -1,12 +1,64 @@
 package code_gen_test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import joosc.Joosc;
 import utility.FileUtility;
 
 public class CodeGenTestMain {
-	public static void main(String[] args){
+
+    public static void main(String[] args) throws IOException,
+            InterruptedException {
+
+        final String myDir = System.getProperty("user.dir");
+
         String[] paths = new String[0];
-        paths = FileUtility.getFileNames(System.getProperty("user.dir") + "/test/testprogram/TestCode.java").toArray(paths);
+        paths = FileUtility.getFileNames(myDir + "/test/testprogram/TestCode.java").toArray(paths);
         Joosc.compileSTL(paths);
-	}
+
+        callBash();
+    }
+
+    private static void callBash() throws IOException, InterruptedException {
+        String myDir = System.getProperty("user.dir");
+        String command = "bash " + myDir + "/test/runnasm.sh";
+        // String command = "ls";
+        final Process p = Runtime.getRuntime().exec(command);
+        StringBuilder sb = new StringBuilder();
+        Thread getOutPut = new Thread() {
+            public void run() {
+                String s = "";
+                BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                try {
+                    while ((s = stdInput.readLine()) != null) {
+                        sb.append(s + "\n");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        getOutPut.start();
+
+        Thread getError = new Thread() {
+            public void run() {
+                String s = "";
+                BufferedReader stdError = new BufferedReader( new InputStreamReader(p.getErrorStream()));
+                try {
+                    while ((s = stdError.readLine()) != null) {
+                        sb.append(s + "\n");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        getError.start();
+        getOutPut.join();
+        getError.join();
+        p.waitFor();
+        System.out.println(sb.toString());
+    }
  }
