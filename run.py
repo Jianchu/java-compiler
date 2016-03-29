c8ae6140bc98d3ee3117fc84646e20cf6d9d533c
z72lin <z72lin@student.cs.uwaterloo.ca>
2016-03-29 19:52:56 -0400

    python script

---
+++ run.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+
+def getAllPath(l, path):
+    for root, directories, filenames in os.walk(path):
+        for f in filenames:
+            fn = os.path.join(root, f)
+            l.append(fn)
+            
+def getStdLib():
+    path = 'java'
+    l = list()
+    getAllPath(l, path)
+    return l
+
+if __name__=='__main__':
+    program = './joosc'
+    l = getStdLib()
+    program = [program] + l
+    #print(program)
+    p = subprocess.Popen(program)
+    p.wait()
+
+    for root,_,filenames in os.walk('output'):
+        for f in filenames:
+            fn = os.path.join(root, f)
+            nasm = subprocess.Popen('/u/cs444/bin/nasm -O1 -f elf -g -F dwarf'.split() + [fn])
+            nasm.wait()
+    ld = subprocess.Popen('ld -melf_i386 -o main output/*.o'.split())
+    ld.wait()
+
+    
+    
