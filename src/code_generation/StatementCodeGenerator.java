eb0fb9ffb6d3cc7b1abf1dc6a8147e838dd82d1c
zane <zane.z.lin@gmail.com>
2016-03-29 21:24:16 -0400

    fix typo

--- src/code_generation/StatementCodeGenerator.java
+++ src/code_generation/StatementCodeGenerator.java
@@ -82,13 +82,13 @@ public class StatementCodeGenerator extends TraversalVisitor {
 
         appendNode(forText, node.forInit, expGen);
         StringUtility.appendLine(forText, "jmp COND_" + n);
-        StringUtility.appendLine(forText, "LOD_" + n + ":");
+        StringUtility.appendLine(forText, "LOOP_" + n + ":");
         appendNode(forText, node.forBody, this);
         appendNode(forText, node.forUpdate, expGen);
         StringUtility.appendLine(forText, "COND_" + n + ":");
         appendNode(forText, node.forCondition, expGen);
-        StringUtility.appendLine(forText, "cmp eak, " + FALSE);
-        StringUtility.appendLine(forText, "jne Loop_" + n);
+        StringUtility.appendLine(forText, "cmp eax, " + FALSE);
+        StringUtility.appendLine(forText, "jne LOOP_" + n);
 
         node.attachCode(forText.toString());
     }
