c32bbd8be483cd8f46326e26c00abcb546e4aa28
Jianchu Li <j538li@uwaterloo.ca>
2016-04-01 10:00:49 -0400

    tests

--- test/testprogram/code_gen/TestArithmetic.java
+++ test/testprogram/code_gen/TestArithmetic.java
@@ -1,12 +1,8 @@
-public class TestLocalDec {
-    public TestLocalDec() {}
+public class TestArithmetic {
+    public TestArithmetic() {}
     public static int test()  {
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        int d = 4;
-        int result = 0;
-        
-        return result;
+        int i = 0;
+        System.out.println(i/i);
+        return 123;
     }
 }
